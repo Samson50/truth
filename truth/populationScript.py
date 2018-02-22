c08c171075efe67d182a495ed51b353d892129fc
Jacob Weiss <jakeweiss50@gmail.com>
2018-02-21 19:23:01 -0500

    Added DB backup and restore functions

--- truth/populationScript.py
+++ truth/populationScript.py
@@ -1,14 +1,43 @@
+import os
+
 from congressScraper import CongressScraper
 from dbManager import DBManager
 
-manager = DBManager()
-manager.recreateAll()
-manager.close()
-congress = CongressScraper()
-congress.populateDB()
-congress.close()
+def clean():
+    manager = DBManager()
+    manager.recreateAll()
+    manager.close()
+
+def fillCongress():
+    congress = CongressScraper()
+    congress.populateDB()
+    congress.close()
+
+def backupDB():
+    DB_USER = raw_input("User Name: ")
+    DB_PASSWORD = raw_input("Password: ")
+    DB_NAME = raw_input("Database: ")
+    BACKUPPATH = os.getcwd()
+    BACKUP_NAME = raw_input("Backup Name: ")
+    dumpcmd = "mysqldump -u "+DB_USER+" -p "+DB_PASSWORD+" "+DB_NAME+" > "+BACKUPPATH+"\\"+BACKUP_NAME+".sql"
+    os.system(dumpcmd)
+
+def restoreDB():
+    DB_USER = raw_input("User Name: ")
+    DB_PASSWORD = raw_input("Password: ")
+    DB_NAME = raw_input("Database: ")
+    BACKUPPATH = os.getcwd()
+    BACKUP_NAME = raw_input("Backup Name: ")
+    #os.chdir("C:\\Program Files\\MySQL\\MySQL Server 5.7\\bin")
+    dumpcmd = "mysqldump -u "+DB_USER+" -p "+DB_PASSWORD+" "+DB_NAME+" < "+BACKUPPATH+"\\"+BACKUP_NAME+".sql"
+    os.system(dumpcmd)
+
 #mysqldump -u root -p --databases federal > fed_bu.sql
 #mysql -u root -p  federal < fed_bu.sql
 
 #run tests from C:\Users\Jacob\git\truth
 #restore database from ADMIN C:\Program Files\MySQL\MySQL Server 5.7\bin
+
+#import os
+#dumpcmd = "mysqldump -u "+DB_USER+" -p "+DB_PASSWORD+" "+DB_NAME+" > "+BACKUPPATH+"/"+BACKUP_NAME+".sql"
+#os.system(dumpcmd)
